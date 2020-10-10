0a3346f1da138b48cfc1ffecc9a77f1ac0a93c00
Jonmlr <57244292+Jonmlr@users.noreply.github.com>
2020-10-10 15:42:17 -0430

    Cambios

--- UNA/routes/web.php
+++ UNA/routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/welcome', function () {
+    return view('welcome');
 });
 
 Auth::routes();
