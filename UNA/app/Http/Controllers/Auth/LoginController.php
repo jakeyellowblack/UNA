8746e666547dd2ea40458953ce7ec907facc8970
Jonmlr <57244292+Jonmlr@users.noreply.github.com>
2020-10-29 04:09:15 -0400

    Cuenta activa/inactiva message

--- UNA/app/Http/Controllers/Auth/LoginController.php
+++ UNA/app/Http/Controllers/Auth/LoginController.php
@@ -40,11 +40,38 @@ class LoginController extends Controller
        $this->middleware('guest')->except('logout');
     }
 	
-	protected function credentials(Request $request)
-{        
-   return ['code' => $request->{$this->username()}, 'password' => $request->password, 'status' => 1];
-}
+    protected function credentials(Request $request)
+    {
+        $credentials =  $request->only($this->username(), 'password', 'status');
+        $credentials['status'] = 1;
+
+        return $credentials;
+    }	
 	
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        // Load user from database
+        $user = \App\User::where($this->username(), $request->{$this->username()})->first();
+
+        // Check if user was successfully loaded, that the password matches
+        // and active is not 1. If so, override the default error message.
+        if ($user && \Hash::check($request->password, $user->password) && $user->status != 1) {
+            $errors = [$this->username() => 'Tu cuenta no está activada.'];
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
+    }	
+	
+
+
 
 	
 }
