2a338b9762a57fb40d60cc1628481a42641f6232
Jonmlr <57244292+Jonmlr@users.noreply.github.com>
2020-11-04 07:57:27 -0400

    Nomina index

--- UNA/resources/views/Nomina/index.blade.php
+++ UNA/resources/views/Nomina/index.blade.php
@@ -26,6 +26,105 @@
 	<!-- Body -->
 <body>
 
+	<!-- Content -->
+	<div class="u-content">
+		<!-- Content Body -->
+		<div class="u-body">
+			<h1 class="h2 mb-2">Nómina</h1>
+
+			<!-- Card -->
+	        <div class="card mb-5">
+		        
+		        <!-- Card Header -->
+
+		        <header class="card-header">
+			        <h2 class="h4 card-header-title">Tabla de Nómina</h2>
+                    
+                    
+		        </header>
+		        <!-- End Card Header -->
+
+		        <!-- Crad Body -->
+	          <div class="card-body pt-0">
+		          <!-- Table -->
+		          <div class="table-responsive">
+			          <table class="table table-hover mb-0">
+				          <thead>
+				          <tr>
+				          	  <th>Nombre</th>
+					          <th>Titulo</th>
+					          <th>Fecha</th>
+
+					          <th class="text-center">Acciones</th> 
+			          </tr>
+				          </thead>
+
+
+
+
+
+			            @foreach($nomina as $no)
+
+			            <tbody>
+				            <tr>
+					          <td class="font-weight-semi-bold"></td>
+					          <td class="font-weight-semi-bold"></td>
+				              <td class="font-weight-semi-bold"></td>
+
+					          <td class="text-center">
+
+                               <!-- Actions --> 
+						          
+						          <div class="dropdown">
+							          <a id="basicTable1MenuInvoker" class="u-icon-sm link-muted" href="#" role="button" aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" data-offset="8">
+								          <span class="ti-more"></span>
+							          </a>
+                                 
+                                     
+							          <!-- Actions Menu -->
+						            <div class="dropdown-menu dropdown-menu-right" style="width: 150px;">
+								        <div class="card border-0 p-3">
+                                          
+									          
+                                              <ul class="list-unstyled mb-0">
+                                            
+										          <li class="mb-3">
+											          <a class="d-block link-dark" href="#editModal" data-mynamefile="{{$ref->namefile}}" data-mytitle="{{$ref->title}}"  data-mydate="{{$ref->date}}" data-usid="{{$ref->id}}" data-toggle="modal">Editar</a>
+										          </li>
+                                                  
+                                                  
+										          <li>
+											          <a class="d-block link-dark" href="#deleteModal" data-usid="{{$ref->id}}" data-toggle="modal">Eliminar</a>
+										          </li>
+									          </ul>
+                                              
+								        </div>
+							        </div>
+							          <!-- End Actions Menu -->
+						          </div>
+						          <!-- End Actions -->
+
+					          </td>
+				          </tr>
+				          </tbody>
+				         
+				@endforeach
+			          </table>
+                      
+		          </div>
+		          <!-- End Table -->
+	          </div>
+		        <!-- Crad Body -->
+	        </div>
+					<!-- End Card -->
+
+					
+						<!-- Crad Body -->
+			  </div>
+					<!-- End Card -->
+</div>
+				<!-- End Content Body -->
+
 
 @include('layouts.footer')
 
@@ -38,5 +137,3 @@
 
 
 @endsection
-
-
