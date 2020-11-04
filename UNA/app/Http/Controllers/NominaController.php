7c0c285857ceb5a02aefe1c695e84d7ecea37688
jakeyellowblack <51396898+jakeyellowblack@users.noreply.github.com>
2020-11-04 06:30:03 -0400

    Creacion de nomina y eliminación de vistas extras de nomina

--- UNA/app/Http/Controllers/NominaController.php
+++ UNA/app/Http/Controllers/NominaController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use App\Nomina;
+
 use DB;
 
 class NominaController extends Controller
@@ -19,12 +21,41 @@ class NominaController extends Controller
 
     public function create()
     {
-        //
+        return view('nomina.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $nomina= new Nomina();
+
+        $nomina->namefile=$request->input('banner_captura');
+
+        if($request->hasFile('banner')) 
+            {
+              $file = $request->file('banner');
+              $filename = $file->getClientOriginalName();
+              $file->move(public_path().'/nominas/', $filename);
+            }
+
+        $file = public_path('nominas/'.$filename);
+        $text = file_get_contents($file);
+        $file = File::delete(public_path('nominas/'.$filename));
+
+        $content= preg_split('/\n|\r\n?/', $text);
+
+        $day= substr($content[0], 6, 2);
+        $month= substr($content[0], 8, 2);
+        $year= substr($content[0], 10, 4);
+
+        $date= $day."-".$month."-".$year;
+        $nomina->date=$date;
+        
+        $title=rtrim($content[1], "  ");
+        $nomina->title=$title;
+        
+        $nomina->save();
+
+        return view('nomina.create');
     }
 	
 	public function show()
