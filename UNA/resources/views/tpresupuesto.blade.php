e873e6a858f661a1d894962b9ffa67cdadb9c1f4
Jonmlr <57244292+Jonmlr@users.noreply.github.com>
2020-11-04 01:39:14 -0400

    Edición de la fecha

--- UNA/resources/views/tpresupuesto.blade.php
+++ UNA/resources/views/tpresupuesto.blade.php
@@ -110,7 +110,7 @@
 		                            
 								<div class="form-group">
 									<label for="created_at">Fecha</label>
-									<input id="created_at" name="created_at" maxlength="200" class="form-control form-pill" type="text" placeholder="Fecha"> 
+									<input id="created_at" name="created_at" maxlength="200" class="form-control datetimepicker form-pill" type="date" placeholder="Fecha"> 
 								</div>                                           
 								
 								<button class="btn btn-primary my-20 my-sm-0" type="submit">Guardar</button>
